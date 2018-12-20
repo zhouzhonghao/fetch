10e61fb391bf7ec276999ae00074995929b8d212
zhouzhonghao <zhouzhonghao@gmail.com>
2018-12-20 11:23:13 +0800

    0.5.0

---
+++ src/index.js
@@ -0,0 +1,122 @@
+import '@babel/polyfill';
+import urlUtils from 'url';
+
+const fetch = global.fetch;
+
+let fetchErrorHandler = null;
+
+const Component = async (options) => {
+    let { withCredentials = true, url = '', method = 'GET', body, data, ...others } = options;
+    let urlEntity = urlUtils.parse(url);
+    if (typeof (data) == 'string'
+        || typeof (data) == 'object') {
+        if (method.toUpperCase() == 'GET') {
+            if (data instanceof global.URLSearchParams) {
+                data = data.toString();
+            }
+            if (typeof (data) != 'string') {
+                data = Object.keys(data).map((key) => {
+                    let value = data[key];
+                    if (typeof (value) == 'object') {
+                        value = JSON.stringify(value);
+                    }
+                    return [
+                        global.encodeURIComponent(key),
+                        '=',
+                        global.encodeURIComponent(value)
+                    ].join('');
+                }).join('&');
+            }
+            url = urlUtils.format({
+                protocol: urlEntity.protocol,
+                host: urlEntity.host,
+                auth: urlEntity.auth,
+                pathname: urlEntity.pathname,
+                search: data ? '?' + data : null
+            });
+            body = null;
+            data = null;
+        }
+        else {
+            if (data instanceof global.FormData) {
+                body = data;
+                data = null;
+            }
+            else {
+                if (data instanceof global.URLSearchParams) {
+                    data = data.toString();
+                }
+                if (typeof (data) != 'string') {
+                    data = Object.keys(data).map((key) => {
+                        let value = data[key];
+                        if (typeof (value) == 'object') {
+                            value = JSON.stringify(value);
+                        }
+                        return [
+                            global.encodeURIComponent(key),
+                            '=',
+                            global.encodeURIComponent(value)
+                        ].join('');
+                    }).join('&');
+                }
+                body = data;
+                data = null;
+            }
+        }
+    }
+    try {
+        const result = await fetch(url, {
+            method: method,
+            ...(
+                typeof (body) == 'string'
+                    || body instanceof global.URLSearchParams
+                    || body instanceof global.FormData
+                    || body instanceof global.Blob
+                    || body instanceof global.File
+                    ? (
+                        {
+                            body: body
+                        }
+                    )
+                    : (
+                        typeof (body) == 'object'
+                            ? (
+                                {
+                                    body: JSON.stringify(body)
+                                }
+                            )
+                            : (
+                                {}
+                            )
+                    )
+            ),
+            ...others,
+            ...(
+                withCredentials
+                    ? (
+                        {
+                            credentials: 'include'
+                        }
+                    )
+                    : (
+                        {}
+                    )
+            )
+        });
+        return result;
+    }
+    catch (e) {
+        if (fetchErrorHandler) {
+            await fetchErrorHandler(e);
+        }
+        else {
+            throw e;
+        }
+    }
+};
+
+Component.fetchError = (handler) => {
+    fetchErrorHandler = handler;
+};
+
+export default Component;
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const path = require('path');
+const webpack = require('webpack');
+const resolve = (dir) => {
+    path.join(__dirname, '.', dir);
+};
+
+module.exports = (env, options) => {
+    const isProd = (options.mode || process.env.NODE_ENV) == 'production';
+    return {
+        mode: isProd ? 'production' : 'development',
+        entry: {
+            index: [
+                './src/index.js'
+            ]
+        },
+        output: {
+            path: path.join(__dirname, './lib'),
+            filename: isProd ? '[name].min.js' : '[name].js',
+            libraryTarget: 'umd',
+            library: 'RFetch',
+            libraryExport: 'default',
+            globalObject: 'this'
+        },
+        devtool: 'hidden-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: [
+                        /node_modules/
+                    ],
+                    use: [
+                        ...(
+                            isProd
+                            ? (
+                                []
+                            )
+                            : ([
+                                {
+                                    loader: 'js-prettier-loader',
+                                    options: {
+                                        parser: 'babylon'
+                                    }
+                                }
+                            ])
+                        ),
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [
+                                    [
+                                        '@babel/preset-env'
+                                    ]
+                                ],
+                                plugins: [
+                                    [
+                                        '@babel/plugin-proposal-decorators',
+                                        {
+                                            decoratorsBeforeExport: false
+                                        }
+                                    ],
+                                    [
+                                        '@babel/plugin-proposal-class-properties',
+                                        {
+                                            loose: true
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+};
